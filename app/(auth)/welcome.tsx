6772ebc49b584a14db3749039de9397bd975745d
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-08-17 01:37:47 +0530

    Refactor Welcome component to integrate onboarding data with Swiper

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,10 +1,12 @@
+import { onboarding } from "@/contants";
 import { router } from "expo-router";
-import React, { useRef } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import React, { useRef, useState } from "react";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 export default function Welcome() {
   const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   return (
     <SafeAreaView className="flex h-full bg-white items-center justify-between">
       <TouchableOpacity
@@ -22,16 +24,27 @@ export default function Welcome() {
         activeDot={
           <View className="w-[32px] h-[4px] mx-1 bg-[#2F74FA] rounded-[2px]" />
         }
+        onIndexChanged={(index) => setActiveIndex(index)}
       >
-        <View>
-          <Text>hello 1</Text>
-        </View>
-        <View>
-          <Text>hello 2</Text>
-        </View>
-        <View>
-          <Text>hello 3</Text>
-        </View>
+        {onboarding?.map((item, index) => (
+          <View key={index} className="flex justify-center items-center mt-3">
+            <Image
+              source={item?.image}
+              className="w-full h-[300px]"
+              resizeMode="contain"
+            />
+            <View className="flex flex-row justify-center items-center mt-10 text-black">
+              <Text className="font-bold text-3xl mx-10 text-center">
+                {" "}
+                {item?.title}
+              </Text>
+            </View>
+            <Text className="font-JakartaSemiBold text-lg mx-10 text-center mt-3 text-[#858585]">
+              {" "}
+              {item?.description}
+            </Text>
+          </View>
+        ))}
       </Swiper>
     </SafeAreaView>
   );
