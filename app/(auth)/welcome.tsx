cd141ff6fd5dc60219bded785a6ece8622d122c4
Abhishek Katnoria <abhishekkatnoria993@gmail.com>
2025-08-18 00:00:45 +0530

    Added a button

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/contants";
 import { router } from "expo-router";
 import React, { useRef, useState } from "react";
@@ -5,8 +6,9 @@ import { Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Swiper from "react-native-swiper";
 export default function Welcome() {
-  const swiperRef = useRef<Swiper>(null);
+  const swiperRef = useRef<Swiper | null>(null);
   const [activeIndex, setActiveIndex] = useState<number>(0);
+  const lastSlideIndex = activeIndex === onboarding.length - 1;
   return (
     <SafeAreaView className="flex h-full bg-white items-center justify-between">
       <TouchableOpacity
@@ -33,7 +35,7 @@ export default function Welcome() {
               className="w-full h-[300px]"
               resizeMode="contain"
             />
-            <View className="flex flex-row justify-center items-center mt-10 text-black">
+            <View className="flex flex-row justify-center items-center mt-12 text-black">
               <Text className="font-bold text-3xl mx-10 text-center">
                 {" "}
                 {item?.title}
@@ -46,6 +48,15 @@ export default function Welcome() {
           </View>
         ))}
       </Swiper>
+      <CustomButton
+        title={lastSlideIndex ? "Get Started" : "Next"}
+        onPress={() =>
+          lastSlideIndex
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef?.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 }
